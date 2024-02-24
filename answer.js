dfbc05aeaa5ee7ba0c33734df3ef6658430229f2
reyhanehkarimi <reyhanehkarimi9978@gmail.com>
2024-02-24 11:41:54 +0330

    answer-js2

---
+++ answer.js
@@ -0,0 +1,37 @@
+//1
+// let firstName = "reyhaneh";
+// console.log(firstName.toUpperCase());
+
+//2
+// let myName = "ReyhAneH"
+// console.log(myName.toLowerCase());
+
+//3
+// let nameMe = "reyhan"
+// console.log(nameMe.length);
+
+//4
+// let fullName = "  reyhanehkarimi  "
+// console.log(fullName.trim());
+
+//5
+// let firstName = "Reyhaneh"
+// let lastName = "Karimi"
+// fullName = firstName + lastName;
+// console.log(fullName);
+
+//6
+// let myName = "reyhanehkarimi"
+// // let lastName = ;
+// console.log(myName[8] + myName[9] + myName[10] + myName[11] + myName[12] + myName[13]);
+
+//7
+// const lorem = "Lorem ipsum\", or \"lipsum\" as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of \"Cicero's De Finibus Bonorum et Malorum\" for use in a type specimen book."
+// console.log(lorem);
+
+//8
+// const abc = 'abcdefghijklmnopqrstuvwxyz'
+// console.log(abc[17] + abc[4] + abc[24] + abc[7] + abc[0] + abc[13]);
+
+// const abc = 'abcdefghijklmnopqrstuvwxyz'
+// console.log(abc[0].toUpperCase() + abc[15].toUpperCase() + abc[18].toUpperCase() + abc[8] + abc[6] + abc[13] + abc[0] + abc[11] + abc[18]);
